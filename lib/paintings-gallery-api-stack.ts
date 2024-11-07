8499cf68f826cc0a3f3d0e1a60438dfa7c230246
TheMarkRyan <20098241@mail.wit.ie>
2024-11-07 18:34:37 +0000

    Define DynamoDB table for paintings with composite primary key

--- lib/paintings-gallery-api-stack.ts
+++ lib/paintings-gallery-api-stack.ts
@@ -1,8 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
-const paintingsTable = new dynamodb.Table(this, 'PaintingsTable', {
-  partitionKey: { name: 'paintingId', type: dynamodb.AttributeType.STRING },
-  sortKey: { name: 'reviewId', type: dynamodb.AttributeType.STRING },
-  billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-  removalPolicy: cdk.RemovalPolicy.DESTROY, // Change to RETAIN for production
-});
+export class PaintingsGalleryApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB Table for Paintings and Reviews
+    const paintingsTable = new dynamodb.Table(this, 'PaintingsTable', {
+      partitionKey: { name: 'paintingId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'reviewId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+    });
+
+
+}}
